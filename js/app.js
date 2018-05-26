ae964db3b3e36e9d59ad93ed3b8f8f9a96889b93
artnerdnet <36138541+artnerdnet@users.noreply.github.com>
2018-05-26 07:29:38 -0300

    Add files via upload
    defined variables
    added movement 
    drew player character

--- js/app.js
+++ js/app.js
@@ -1,35 +1,91 @@
+
+
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+class Enemy {
+        constructor(x=1, y=2, speed) {
+          this.x = x;
+          this.y = y;
+          this.speed = speed;
+          this.sprite = 'images/enemy-bug.png';
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+        }
+        render() { // Draw the enemy on the screen, required method for game
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+        update(dt) {
+            speed = this.x * dt;
+           // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+        } 
+      }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+class Player {
+        constructor(x=2, y=1, speed) {
+          this.x = x;
+          this.y = y;
+          this.speed = speed;
+          this.sprite = 'images/char-boy.png';
+        }
 
+        render() { // Draw the player on the screen, required method for game
+            ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        }
+
+        update(dt) {
+            
+           // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+        } 
+
+
+    handleInput(keyPressed) {
+
+        if (keyPressed == 'left') {
+            this.x = this.x -= 101; 
+            if (this.x <= 0) {
+                this.x = 0;
+            }
+        } else if (keyPressed == 'up') {
+            this.y = this.y -= 83;
+            if (this.y <= 0) { 
+                this.y= this.y % 83;
+            }
+        } else if (keyPressed == 'right') {
+            this.x = this.x += 101;
+            if (this.x >= 400) {
+                this.x= 400;
+            }
+        }  else if (keyPressed == 'down') {
+            this.y = this.y += 83;
+            if (this.y >= 400) {
+                this.y= 370;
+            }
+        }
+
+    }
+
+}
+
+
+
+    
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-
 // Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+let player = new Player(100,204);
+let enemy = new Enemy(200,300); 
 
+allEnemies = []
+/* this for the enemies inside the arrays, for reset and for lvl up
+allEnemies.push(
+
+)*/ 
 
 
 // This listens for key presses and sends the keys to your
